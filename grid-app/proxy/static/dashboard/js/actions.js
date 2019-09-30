960f2418d713262575e91eba5d1d7450e4f66625
anchunyu <421413836@qq.com>
2019-09-30 10:36:51 +0800

    Update actions.js

--- grid-app/proxy/static/dashboard/js/actions.js
+++ grid-app/proxy/static/dashboard/js/actions.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
     });
 
     $(document).on("click", "#register", function(){
-        window.location.replace("/register");
+        window.location.assign("/register");
     });
 
     function getCookie(cname) {
